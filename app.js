e73b10bc9a1c54c7c5f0064e957aa5333ce4a5d5
BMayank0405 <mayankbarthwal58@gmail.com>
2018-01-05 22:52:57 +0530

    deleting phpmailer just using it on the server

---
+++ app.js
@@ -0,0 +1,5 @@
+(function () {
+   'use strict';
+   var about_us = "<span><b>E</b>ven</span><span><b>C</b>argo</span>is a social enterprise that < strong > employs women</strong > from resource poor communities and trains them for employment opportunities with the major e - commerce companies.<br/> <br/>Our focus is to overcome the barriers of unemployment through < strong > skill development of women</strong > to increase the participation of women workforce in labour market.";
+   $('.about-text > p').textContent = about_us;
+})();
